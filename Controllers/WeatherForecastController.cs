828183f1e9440bca330ede988cf7f0220f613c46
setupcopy <setupcopy@gmail.com>
2022-03-09 13:47:06 +1100

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+					 	var abc = "123";
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
